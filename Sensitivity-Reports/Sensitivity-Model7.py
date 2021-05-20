1d7a287acb65c7f4cf46273f27d53f4f5277386c
Marisa <anam.macedo98@gmail.com>
2021-05-20 19:38:58 +0100

    small changes

--- Sensitivity-Reports/Sensitivity-Model7.py
+++ Sensitivity-Reports/Sensitivity-Model7.py
@@ -5,7 +5,7 @@ import gurobipy as gp
 
 # Read model
 
-model = gp.read("../Resultados/constraints-P6.lp")
+model = gp.read("../Modelos/P6.lp")
 
 # Solve model
 model.optimize()
@@ -17,13 +17,13 @@ if model.status != GRB.OPTIMAL:
 
 print('\n\n--- SENSITIVITY REPORT ---\n\n')
 
-print('Função Objetivo = {}\n'.format(model.objVal))
+print('Valor da Função Objetivo = {}\n'.format(model.objVal))
 
 print('Variables\nName\tFinal Value\tReduced cost\tObjective Coefficient\tCoefficient Lower Bound\tCoefficient Upper Bound')
 for v in model.getVars():
     print('{}\t{}\t{}\t{}\t{}\t{}'.format(v.varName, v.x, v.RC, v.Obj, v.SAObjLow, v.SAObjUp))
 
 
-print('Constraints\nName\tShadow Price\tSlack\tRHS\tRHS Up\tRHS Low')
+print('\nConstraints\nName\tShadow Price\tSlack\tRHS\tRHS Up\tRHS Low')
 for c in model.getConstrs():
     print('{}\t{}\t{}\t{}\t{}\t{}'.format(c.ConstrName, c.Pi, c.Slack, c.RHS, c.SARHSUp, c.SARHSLow))
