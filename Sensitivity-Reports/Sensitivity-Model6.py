ee85ed2b7b71f5b39b95a1f07b79c3f5551f1738
Ana Macedo <61831138+anamarisamacedo@users.noreply.github.com>
2021-05-31 20:12:19 +0100

    Update Sensitivity-Model6.py

--- Sensitivity-Reports/Sensitivity-Model6.py
+++ Sensitivity-Reports/Sensitivity-Model6.py
@@ -14,16 +14,17 @@ if model.status != GRB.OPTIMAL:
     print('Optimization ended with status %d' % model.status)
     sys.exit(0)
 
-
+# Generate Sensitivity Report
 print('\n\n--- SENSITIVITY REPORT ---\n\n')
 
 print('Valor da Função Objetivo = {}\n'.format(model.objVal))
 
+# Variables
 print('Variables\nName\tFinal Value\tReduced cost\tObjective Coefficient\tAllowable Decrease\tAllowable Increase')
 for v in model.getVars():
     print('{}\t{}\t{}\t{}\t{}\t{}'.format(v.varName, v.x, v.RC, v.Obj, v.SAObjLow, v.SAObjUp))
 
-
+# Constraints
 print('\nConstraints\nName\tShadow Price\tSlack\tRHS\tRHS Up\tRHS Low')
 for c in model.getConstrs():
     print('{}\t{}\t{}\t{}\t{}\t{}'.format(c.ConstrName, c.Pi, c.Slack, c.RHS, c.SARHSUp, c.SARHSLow))
